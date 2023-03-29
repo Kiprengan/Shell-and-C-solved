8bae0bb1a662ff1df781dff01ee143643eb200f6
Denis Kiprono <deniskiprono881@gmail.com>
2023-03-29 23:29:37 +0000

    Updated files

--- shell/backups/backupShellScript.sh
+++ shell/backups/backupShellScript.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
+# This first line specify that this is bash script
 
-# Get command line arguments
-directory_to_copy=$1
-backup_directory=$2
-
+# Getting command line arguments
+source_directory_to_be_backed_up=$1
+# First argument is the given directory to be backed up
+destination_directory_for_backup=$2
+# Second argument is the directory that will work as backup location 
 # Create the backup directory if it doesn't exist
-if [ ! -d "$backup_directory" ]; then
-  mkdir "$backup_directory"
+if [ ! -d "$destination_directory_for_backup" ]; then
+	mkdir "$destination_directory_for_backup"
 fi
 
 # Loop through each file in the directory
-for file in "$directory_to_copy"/*; do
+for file in "$source_directory_to_be_backed_up"/*; do
   # Check if the file is a regular file (not a directory)
   if [ -f "$file" ]; then
     # Check if the file has been modified since the last backup
-    if [ "$file" -nt "$backup_directory/$(basename "$file")" ]; then
-      # Copy the file to the backup directory
-      cp "$file" "$backup_directory"
+    if [ "$file" -nt "$destination_directory_for_backup/$(basename "$file")" ]; then
+      # Copy the file to the backup directory and nt stands for new than which is a argument to check if file has been modified
+      cp "$file" "$destination_directory_for_backup"
       echo "Backed up $file"
     fi
   fi
